7efe5233135d34c373e4d595fa0d4df50b11ee80
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-03-23 07:30:07 +0200

    This program shows all the colors created by the RGB LED

--- main.ts
+++ main.ts
@@ -10,35 +10,47 @@ pins.digitalWritePin(DigitalPin.P13, 0)
 pins.digitalWritePin(DigitalPin.P14, 0)
 pins.digitalWritePin(DigitalPin.P15, 0)
 basic.showIcon(IconNames.Happy)
+
 input.onButtonPressed(Button.A, function () {
-  pins.digitalWritePin(DigitalPin.P13, 1)
-   
-    basic.showString('Red')
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 1)
-    
-    basic.showString('Blue')
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    
-    basic.showString('Green')
-    pins.digitalWritePin(DigitalPin.P15, 0)
+
     pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 1)
-    
-    basic.showString('Magenta')
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    
-    basic.showString('Yellow')
+    basic.showString("Red")
+    basic.pause(1000)
 
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 1)
-    
-    basic.showString('Cyan')
+    basic.showString("Blue")
+    basic.pause(1000)
+
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString("Green")
+    basic.pause(1000)
+
     pins.digitalWritePin(DigitalPin.P13, 1)
-    
-    basic.showString('White')
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.showString("Magenta")
+    basic.pause(1000)
+
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString("Yellow")
+    basic.pause(1000)
+
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString("Cyan")
+    basic.pause(1000)
+
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString("White")
+    basic.pause(1000)
+
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
