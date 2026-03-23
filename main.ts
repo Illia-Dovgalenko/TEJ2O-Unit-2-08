7207eadf5c40278efe5429f0c35d4cd8ed388a28
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-03-23 07:14:16 +0200

    all the colors created by the RGB LED

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+*
+* Created by: Illia
+* Created on: March 2026
+* This program shows all the colors created by the RGB LED
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+basic.showIcon(IconNames.Happy)
+input.onButtonPressed(Button.A, function () {
+  pins.digitalWritePin(DigitalPin.P13, 1)
+   
+    basic.showString('Red')
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    
+    basic.showString('Blue')
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    
+    basic.showString('Green')
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    
+    basic.showString('Magenta')
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    
+    basic.showString('Yellow')
+
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    
+    basic.showString('Cyan')
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    
+    basic.showString('White')
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.showIcon(IconNames.Happy)
+})
